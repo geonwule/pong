1c54fc8ce79d5d8d04a297f451a02add13ef9480
geonwule <geonwlee22@gmail.com>
2024-05-02 17:17:57 +0900

    :memo: docs: main함수 공부용으로 수정 및 주석 추가

--- srcs/Client/srcs/main.cpp
+++ srcs/Client/srcs/main.cpp
@@ -1,143 +1,110 @@
 #include <iostream>
 #include <string>
-#include <sys/socket.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <future>
 #include <thread>
 #include "../../../mac_opengl/include/GLFW/glfw3.h"
 
+#define WIDTH 1500
+#define HEIGHT 750
+
 using namespace std;
 
-void func(int sockfd)
+void error_callback(int error, const char *description)
 {
-    char buffer[1024] = {0};
-    while (1)
+    std::cerr << "GLFW Error (" << error << "): " << description << std::endl;
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+// 키 콜백함수 정의
+static void key_callback(GLFWwindow *window, int key, int scancode, int action, int mods)
+{
+    (void)scancode;
+    (void)mods;
+    if (action == GLFW_PRESS)
     {
-        int valread = recv(sockfd, buffer, 1024, 0);
-        if (valread == -1)
+        switch (key)
         {
-            cerr << "Failed to receive message" << endl;
-            return;
+        case GLFW_KEY_W:
+            cout << "W key pressed" << endl;
+            break;
+        case GLFW_KEY_S:
+            cout << "S key pressed" << endl;
+            break;
+        case GLFW_KEY_ESCAPE:
+            cout << "ESC key pressed" << endl;
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+            break;
+        default:
+            cout << "Other key pressed" << endl;
+            break;
         }
-        buffer[valread] = 0;
-        cout << buffer << endl;
+    }
+    else if (action == GLFW_RELEASE)
+    {
+        switch (key)
+        {
+        case GLFW_KEY_W:
+            cout << "W key released" << endl;
+            break;
+        case GLFW_KEY_S:
+            cout << "S key released" << endl;
+            break;
+        }
     }
 }
 
-void funcGlfw()
+void studyGlfw()
 {
-    if (!glfwInit()) {
+    // 오류 콜백 함수 설정
+    glfwSetErrorCallback(error_callback);
+
+    if (!glfwInit())
+    {
         return;
     }
 
-    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
+    GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
+    if (!window)
+    {
         glfwTerminate();
-        return ;
+        return;
     }
 
     glfwMakeContextCurrent(window);
+    // 프레임버퍼 사이즈 콜백 설정
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    // 키 콜백 설정
+    glfwSetKeyCallback(window, key_callback);
+    double time;
+    while (!glfwWindowShouldClose(window))
+    {
+        //시간 측정
+        time = glfwGetTime();
 
-    while (!glfwWindowShouldClose(window)) {
+        //백 버퍼 초기화
         glClear(GL_COLOR_BUFFER_BIT);
+
+        /* 그림 그리기 (백버퍼에)
+        이벤트 처리가 많은 시간을 소모하는 작업이라면, 
+        그림 그리는 작업과 이벤트 처리를 별도의 스레드에서 수행하는 것이 더 효율적일 수 있습니다*/
+
+        // 백버퍼와 프론트 버퍼 교환
         glfwSwapBuffers(window);
+
+        // 이벤트 처리
+        // 앞쪽에서 처리해줘도 문제 X
         glfwPollEvents();
     }
 
     glfwTerminate();
-    return ;
+    return;
 }
 
-void func2(int sockfd)
+int main()
 {
-    while (1)
-    {
-        string message;
-        cout << "Enter message: ";
-        getline(cin, message);
-        message += '\n';
-
-        if (message == "exit")
-            break;
-
-        if (send(sockfd, message.c_str(), message.size(), 0) == -1)
-        {
-            cerr << "Failed to send message" << endl;
-            return;
-        }
-        else
-        {
-            cout << "Message sent: " << message << endl;
-        }
-    }
-}
-
-int main(int ac, char **av)
-{
-    if (ac != 3)
-    {
-        cerr << "Usage: " << av[0] << " <ip> <port_num>" << endl;
-        return 1;
-    }
-
-    string ip = av[1];
-    int port_num = stoi(av[2]);
-
-    // Create a socket
-    int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1)
-    {
-        cerr << "Failed to create socket" << endl;
-        return 1;
-    }
-
-    // Set up the server address
-    // sockaddr_in serverAddr{}; C++11
-    sockaddr_in serverAddr;
-    memset(&serverAddr, 0, sizeof(serverAddr));
-    serverAddr.sin_family = AF_INET;
-    serverAddr.sin_port = htons(port_num);
-    if (inet_pton(AF_INET, ip.c_str(), &(serverAddr.sin_addr)) <= 0)
-    {
-        cerr << "Invalid address/Address not supported" << endl;
-        return 1;
-    }
-
-    // Connect to the server
-    if (connect(sockfd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)) < 0)
-    {
-        cerr << "Connection failed" << endl;
-        return 1;
-    }
-
-    std::thread t1(func, sockfd);
-    std::thread t2(func2, sockfd);
-    funcGlfw();
-    // std::thread t2(funcGlfw);
-
-    while (1)
-    {
-        string message;
-        cout << "Enter message: ";
-        getline(cin, message);
-        message += '\n';
-
-        if (message == "exit")
-            break;
-
-        if (send(sockfd, message.c_str(), message.size(), 0) == -1)
-        {
-            cerr << "Failed to send message" << endl;
-            return 1;
-        }
-        else
-        {
-            cout << "Message sent: " << message << endl;
-        }
-    }
-    // t1.join();
-    // t2.join();
-    close(sockfd);
+    studyGlfw();
     return 0;
 }
