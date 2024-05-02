1e9d20e41091b39a1c1d49da672891aff3fc26cd
geonwule <geonwlee22@gmail.com>
2024-05-02 17:26:54 +0900

    :memo: docs: 마우스 이동, 클릭 이벤트 추가

--- srcs/Client/srcs/main.cpp
+++ srcs/Client/srcs/main.cpp
@@ -13,7 +13,7 @@ void error_callback(int error, const char *description)
     std::cerr << "GLFW Error (" << error << "): " << description << std::endl;
 }
 
-void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
 }
@@ -56,6 +56,51 @@ static void key_callback(GLFWwindow *window, int key, int scancode, int action, 
     }
 }
 
+// 마우스 버튼 콜백 함수 정의
+static void mouse_button_callback(GLFWwindow *window, int button, int action, int mods)
+{
+    (void)mods;
+    switch (action)
+    {
+    case GLFW_PRESS:
+        switch (button)
+        {
+        case GLFW_MOUSE_BUTTON_LEFT:
+            cout << "Left button pressed" << endl;
+            break;
+        case GLFW_MOUSE_BUTTON_RIGHT:
+            cout << "Right button pressed" << endl;
+            break;
+        default:
+            cout << "Other button pressed" << endl;
+            break;
+        }
+        break;
+    case GLFW_RELEASE:
+        switch (button)
+        {
+        case GLFW_MOUSE_BUTTON_LEFT:
+            cout << "Left button released" << endl;
+            break;
+        case GLFW_MOUSE_BUTTON_RIGHT:
+            cout << "Right button released" << endl;
+            break;
+        default:
+            cout << "Other button released" << endl;
+            break;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+// 마우스 이동 콜백 함수 정의
+static void cursor_position_callback(GLFWwindow *window, double xpos, double ypos)
+{
+    cout << "Cursor Position at (" << xpos << " : " << ypos << ")" << endl;
+}
+
 void studyGlfw()
 {
     // 오류 콜백 함수 설정
@@ -63,14 +108,14 @@ void studyGlfw()
 
     if (!glfwInit())
     {
-        return;
+        exit(EXIT_FAILURE);
     }
 
     GLFWwindow *window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
-        return;
+        exit(EXIT_FAILURE);
     }
 
     glfwMakeContextCurrent(window);
@@ -78,17 +123,22 @@ void studyGlfw()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     // 키 콜백 설정
     glfwSetKeyCallback(window, key_callback);
+    // 마우스 버튼 콜백 설정
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    // 마우스 이동 콜백 설정
+    glfwSetCursorPosCallback(window, cursor_position_callback);
+
     double time;
     while (!glfwWindowShouldClose(window))
     {
-        //시간 측정
+        // 시간 측정
         time = glfwGetTime();
 
-        //백 버퍼 초기화
+        // 백 버퍼 초기화
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* 그림 그리기 (백버퍼에)
-        이벤트 처리가 많은 시간을 소모하는 작업이라면, 
+        이벤트 처리가 많은 시간을 소모하는 작업이라면,
         그림 그리는 작업과 이벤트 처리를 별도의 스레드에서 수행하는 것이 더 효율적일 수 있습니다*/
 
         // 백버퍼와 프론트 버퍼 교환
