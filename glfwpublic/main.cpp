b9005f2614f6d04031ce1868cd2313483d9eb854
geonwule <geonwlee22@gmail.com>
2024-05-03 17:14:57 +0900

    :memo: docs: glfw 라이브러리 추가
    - glfw라이브러리 공식 깃허브에서 소스코드를 다운로드 받아 추가

---
+++ glfwpublic/main.cpp
@@ -0,0 +1,33 @@
+#include "include/glfw3.h"
+#include <iostream>
+
+
+int main()
+{
+    
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    GLFWwindow* window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        std::cout << "Failed to create window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
