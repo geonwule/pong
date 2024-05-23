a35a518cab5475b3deffeb695ca6c858ecdcd110
geonwule <geonwlee22@gmail.com>
2024-05-23 14:10:33 +0900

    🔨 refactor: playgame 스레드자원 join할수 있도록 수정
    - MatchMaking.hpp : private(addPlayThread), public(monitoringGameplayThread) 추가
    - MatchMaking.cpp : addPlayThread, monitoringGameplayThread 구현
    - main.cpp : 모니터링 스레드 로직 추가 및 메모리 릭 확인 로직 추가
    - Util.cpp : signalHandler에 atom_stop = true 로직 추가(실행중인 스레드 전부 종료시키기 위함)

--- srcs/Server/srcs/MatchMaking.cpp
+++ srcs/Server/srcs/MatchMaking.cpp
@@ -1,10 +1,24 @@
 #include "MatchMaking.hpp"
 #include "Util.hpp"
+#include "Cache.hpp"
 
 std::thread *MatchMaking::_play_thread[MAX_THREAD] = {nullptr};
 size_t MatchMaking::_num_players = 0;
 s_Client *MatchMaking::_players[MAX_CLIENTS] = {0};
-void playGame(s_Client *player1,  s_Client *player2);
+void playGame(s_Client *player1, s_Client *player2);
+
+int MatchMaking::addPlayThread(std::thread *thread)
+{
+    for (int i = 0; i < MAX_THREAD; i++)
+    {
+        if (_play_thread[i] == nullptr)
+        {
+            _play_thread[i] = thread;
+            return EXIT_SUCCESS;
+        }
+    }
+    return EXIT_FAILURE;
+}
 
 void MatchMaking::matchPlayers()
 {
@@ -28,10 +42,36 @@ void MatchMaking::matchPlayers()
 
     if (player1 == nullptr || player2 == nullptr)
         error_msg(FATAL_ERR);
-    
-    Thread::createThread(playGame, player1, player2); //,check_thread_over); playGame이 종료되었음을 알수있는 변수를 넣고 그변수를 저장하고 있는 스레드에서 핸들링 하고 있다가 join해주게 추가 예정
-    //addPlayThread(thread);
+
+    std::thread *thread = Thread::createThread(playGame, player1, player2);
+    if (addPlayThread(thread))
+        error_msg(FATAL_ERR); // 스레드 추가 실패시 에러처리 추가 예정
+
     std::cout << "[MatchMaking::matchPlayers] over" << std::endl;
+}
+
+void MatchMaking::monitoringGameplayThread()
+{
+    size_t log_num = 0;
+    while(!Cache::atom_stop)
+    {
+        log_num++;
+        int cnt = 0;
+        for (int i = 0; i < MAX_THREAD; i++)
+        {
+            if (_play_thread[i] != nullptr)
+            {
+                cnt++;
+                Thread::joinThread(_play_thread[i]);
+                _play_thread[i] = nullptr;
+            }
+        }
+        sleep(1);
+        if (cnt)
+            std::cout << "monitoringGameplayThread...[" << cnt << "] game thread join" << std::endl; //test
+        else if (log_num % 10 == 0)
+            std::cout << "monitoringGameplayThread...no game thread join" << std::endl; //test
+    }
 }
 
 bool MatchMaking::checkDoublePlayer(s_Client *player)
@@ -48,7 +88,7 @@ void MatchMaking::addPlayer(s_Client *player)
 {
     if (player == nullptr || checkDoublePlayer(player))
         return;
-    
+
     for (int i = 0; i < MAX_CLIENTS; i++)
     {
         if (_players[i] == nullptr)
--- srcs/Server/srcs/main.cpp
+++ srcs/Server/srcs/main.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 #include "Server.hpp"
 #include "Util.hpp"
+#include "Thread.hpp"
+#include "MatchMaking.hpp"
+
+#include <cstdlib>
+
+void leaks()
+{
+    system("leaks pong_server");
+}
 
 int main(int ac, char **av)
 {
+    // atexit(leaks);
     std::signal(SIGINT, signalHandler); // Ctrl + C
     std::signal(SIGTERM, signalHandler); // kill
-    std::signal(SIGPIPE, SIG_IGN);
+    std::signal(SIGPIPE, SIG_IGN); //SIGPIPE
     // std::signal(SIGPIPE, signalHandler); //SIGPIPE
 
     if (!(ac == 2 || ac == 3))
@@ -20,6 +30,8 @@ int main(int ac, char **av)
     else // ac == 3
         Server::setInstance(av[1], av[2]);
     
+    Thread::createThread(MatchMaking::monitoringGameplayThread);
+
     Server *server = Server::getInstance();
     server->runServer();
 }
